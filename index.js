2e0de2f54c38f89f0fa28291d876612fe9e1108e
Vighnesh M <50981274+vighnesh1209@users.noreply.github.com>
2022-02-03 20:58:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      location.replace("https://www.w3schools.com")
 
     }
 
